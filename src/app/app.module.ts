adef1b8aa833604131223f2ed7a7def2875d756e
John <antarcticdepths@gmail.com>
2023-06-27 20:28:02 +0200

    initial blueprint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BoardsMenuComponent } from './header/boards-menu/boards-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BoardsMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/boards-menu/boards-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boards-menu',
+  templateUrl: './boards-menu.component.html',
+  styleUrls: ['./boards-menu.component.css']
+})
+export class BoardsMenuComponent {
+
+}
