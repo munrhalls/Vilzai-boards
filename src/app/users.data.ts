a9f3aa4a53824996d7d3cba560b2693cf830eabc
John <antarcticdepths@gmail.com>
2023-06-27 20:55:26 +0200

    initial blueprint

--- src/app/users.data.ts
+++ src/app/users.data.ts
@@ -20,5 +20,41 @@ export const usersData: User[] = [
         },
       ],
     },
+    {
+      id: Math.random() * Math.random(),
+      title: 'Board B',
+      columns: [
+        {
+          title: 'Column One',
+          orderNum: 0,
+          tasks: [
+            {
+              text: 'Task number #1',
+              color: 'silver',
+              orderNum: 0,
+              alottedTime: 10,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: Math.random() * Math.random(),
+      title: 'Board C',
+      columns: [
+        {
+          title: 'Column One',
+          orderNum: 0,
+          tasks: [
+            {
+              text: 'Task number #1',
+              color: 'silver',
+              orderNum: 0,
+              alottedTime: 10,
+            },
+          ],
+        },
+      ],
+    },
   ]),
 ];
