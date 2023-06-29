1ae7039045e6af743dd4fb751c4771624bf71c8c
John <antarcticdepths@gmail.com>
2023-06-29 20:45:32 +0200

    feature select board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './user.model';
-import { BoardSelectHook } from './board.model';
+import { Board, BoardSelectHook } from './board.model';
 import { usersData } from './users.data';
 
 @Component({
@@ -12,6 +12,7 @@ export class AppComponent {
   title = 'kanboard';
   testUser = usersData[0];
   activeUser: User = this.testUser;
+  selectedBoard: Board = this.testUser.boards[0];
   boardSelectHooks: BoardSelectHook[] = this.testUser.boards.map((board) => {
     return {
       id: board.id,
@@ -21,5 +22,9 @@ export class AppComponent {
 
   onBoardSelected(id: number) {
     console.log(id);
+    const selectedBoard = this.testUser.boards.find((board) => board.id === id);
+    if (selectedBoard) {
+      this.selectedBoard = selectedBoard;
+    }
   }
 }
