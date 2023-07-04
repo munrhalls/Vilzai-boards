afc089f4e29cb420cbdc092c7f59d221aa10dc35
John <antarcticdepths@gmail.com>
2023-07-04 15:40:18 +0200

    board-edit

--- src/app/board-edit/board-edit.component.ts
+++ src/app/board-edit/board-edit.component.ts
@@ -10,11 +10,13 @@ export class BoardEditComponent {
   @Input() board = {} as Board | null;
   @Output() boardDisplayModeSet = new EventEmitter();
   @Output() boardUpdated = new EventEmitter<Board>();
+  title: string = this.board!.title;
 
   handleBoardDisplayModeSet() {
     this.boardDisplayModeSet.emit();
   }
   handleBoardUpdated() {
+    this.board!.title = this.title;
     this.boardUpdated.emit(this.board!);
     console.log('boardUpdated');
   }
