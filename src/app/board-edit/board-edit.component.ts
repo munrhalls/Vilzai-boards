dfafb5b4387352009bc6020ee936a46c5e0347f3
John <antarcticdepths@gmail.com>
2023-07-05 08:09:01 +0200

    board-edit

--- src/app/board-edit/board-edit.component.ts
+++ src/app/board-edit/board-edit.component.ts
@@ -13,17 +13,19 @@ export class BoardEditComponent {
   taskColors: string[] = ['silver', 'orange', 'teal'];
 
   onDragStart(event: any, col: Column) {
-    event.dataTransfer.setData('text', col.orderNum);
+    event.dataTransfer.setData('text', this.board!.columns.indexOf(col));
   }
   onDragOver(event: any, col: Column) {
     event.preventDefault();
   }
   onDrop(event: any, col: Column) {
     event.preventDefault();
-    const dropStart = event.dataTransfer.getData('text');
-    console.log(this.board!.columns.map((col) => col.orderNum));
-    console.log('drop start:', dropStart);
-    console.log('drop place:', col.orderNum);
+    const movedIndex = parseInt(event.dataTransfer.getData('text'));
+    const moved = this.board!.columns[movedIndex];
+    this.board!.columns.splice(movedIndex, 1);
+    // deleting the dragged el should work on drop
+    const dropLocationIndex = this.board!.columns.indexOf(col);
+    this.board!.columns.splice(dropLocationIndex, 0, moved);
   }
   handleBoardDisplayModeSet() {
     this.boardDisplayModeSet.emit();
